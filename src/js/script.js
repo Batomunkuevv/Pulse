e7036314269e653b0878dd0613be50c67939e8ea
Kirill Batomunkuev <batomunkuevkirill@yandex.ru>
2022-04-05 09:39:23 +0700

    Make fullscreen and advantages

--- src/js/script.js
+++ src/js/script.js
@@ -1,34 +1,36 @@
 "use strict";
 
-window.addEventListener('DOMContentLoaded', () => {
-	function ibg() {
-		let ibg = document.querySelectorAll(".ibg");
-		for (var i = 0; i < ibg.length; i++) {
-			if (ibg[i].querySelector("img")) {
-				ibg[i].style.backgroundImage =
-					"url(" + ibg[i].querySelector("img").getAttribute("src") + ")";
-			}
-		}
-	}
+window.addEventListener("DOMContentLoaded", () => {
+    function ibg() {
+        let ibg = document.querySelectorAll(".ibg");
+        for (var i = 0; i < ibg.length; i++) {
+            if (ibg[i].querySelector("img")) {
+                ibg[i].style.backgroundImage =
+                    "url(" +
+                    ibg[i].querySelector("img").getAttribute("src") +
+                    ")";
+            }
+        }
+    }
 
-	ibg();
+    ibg();
 
-	const headerBurger = document.querySelector(".header__burger");
-	const headerMenu = document.querySelector(".header__menu");
-	const headerBottom = document.querySelector(".header-bottom");
-	const menuList = document.querySelector(".menu__list");
-	const body = document.body;
+    const headerBurger = document.querySelector(".header__burger");
+    const headerMenu = document.querySelector(".header__menu");
+    const headerBottom = document.querySelector(".header-bottom");
+    const menuList = document.querySelector(".menu__list");
+    const body = document.body;
 
-	headerBurger.addEventListener('click', () => {
-		headerBurger.classList.toggle("active");
-		headerMenu.classList.toggle("active");
-		headerBottom.classList.toggle("active");
-		body.classList.toggle("lock");
-	});
+    headerBurger.addEventListener("click", () => {
+        headerBurger.classList.toggle("active");
+        headerMenu.classList.toggle("active");
+        headerBottom.classList.toggle("active");
+        body.classList.toggle("lock");
+    });
 
-	menuList.addEventListener('click', () => {
-		menuList.classList.remove("active");
-		headerBottom.classList.remove("active");
-		body.classList.toggle("lock");
-	});
+    menuList.addEventListener("click", () => {
+        menuList.classList.remove("active");
+        headerBottom.classList.remove("active");
+        body.classList.toggle("lock");
+    });
 });
